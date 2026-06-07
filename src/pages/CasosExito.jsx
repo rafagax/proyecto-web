fb7805051bdb5adfaf9f63c0f2c2c4a8733b23e2
georgimachado31-lab <georgimachado31@gmail.com>
2026-06-07 02:19:05 -0400

    eliminacion de pag

--- src/pages/CasosExito.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import UnderConstruction from '../components/UnderConstruction';
-
-const CasosExito = () => {
-  return <UnderConstruction title="Casos de Éxito" />;
-};
-
-export default CasosExito;
--- src/pages/DesarrolloWeb.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import UnderConstruction from '../components/UnderConstruction';
-
-const DesarrolloWeb = () => {
-  return <UnderConstruction title="Desarrollo Web y Mantenimiento" />;
-};
-
-export default DesarrolloWeb;
